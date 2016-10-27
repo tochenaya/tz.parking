01122d2d64ed01defb589deaa3694cde4c0b4f9b
Mariya Tochenaya <tochenaya@magenta-technology.ru>
2016-10-27 22:46:07 +0400

    EntityManager

--- src/main/java/tz/parking/service/ParkingService.java
+++ src/main/java/tz/parking/service/ParkingService.java
@@ -1,11 +1,11 @@
 package tz.parking.service;
 
-import org.hibernate.Session;
-import tz.parking.db.HibernateUtil;
 import tz.parking.entity.Order;
 import tz.parking.entity.Vehicle;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -18,35 +18,31 @@ public class ParkingService {
     private static final Integer PARKING_CAPACITY = 10;
     private static final Integer COST_PER_HOUR = 100;
 
-    public List<Vehicle> getParkingVehicles() {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        List<Vehicle> vehicles = session.createQuery("select o.vehicle from Order o where o.departureTime is null").list();
-        session.close();
-        return vehicles;
+    @PersistenceContext(unitName = "parkingPU")
+    private EntityManager entityManager;
 
+    public List<Vehicle> getParkingVehicles() {
+        return entityManager.createQuery("select o.vehicle from Order o where o.departureTime is null", Vehicle.class)
+                .getResultList();
     }
 
     public Boolean vehicleIsParking(String number) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Long l = (Long) session.createQuery("select count(o.vehicle) from Order o where o.departureTime is null and o.vehicle.number = :number").
-                setParameter("number", number).uniqueResult();
-        session.close();
+        Long l = entityManager.createQuery("select count(o.vehicle) from Order o " +
+                "where o.departureTime is null and o.vehicle.number = :number", Long.class)
+                .setParameter("number", number).getSingleResult();
         return l != 0;
     }
 
     public Integer getNumberOfFreePlaces() {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Long l = (Long) session.createQuery("select count(o) from Order o where o.departureTime is null").uniqueResult();
-        session.close();
+        Long l = entityManager.createQuery("select count(o) from Order o where o.departureTime is null", Long.class).getSingleResult();
         return PARKING_CAPACITY - l.intValue();
     }
 
     //date with time 00:00:00
     public Integer getProfitPerDay(@NotNull Date date) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-
-        List<Order> orders = session.createQuery("select o from Order o where DATE(o.arrivalTime) <= :date AND (o.departureTime is null OR DATE(o.departureTime) >= :date)")
-                .setParameter("date", date).list();
+        List<Order> orders = entityManager.createQuery("select o from Order o where DATE(o.arrivalTime) <= :date " +
+                "AND (o.departureTime is null OR DATE(o.departureTime) >= :date)", Order.class)
+                .setParameter("date", date).getResultList();
 
         LocalDate input = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         LocalDate now = new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
@@ -69,7 +65,6 @@ public class ParkingService {
             int hours = (int) Math.ceil((finish.getTime() - start.getTime()) / 1000 / 60 / 60d);
             sum += hours * COST_PER_HOUR;
         }
-        session.close();
         return sum;
     }
 }
